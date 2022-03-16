91db65633dcc46880e4c1518f7de30023cb2b50e
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-16 13:20:04 -0400

    Make function to toggle mode

--- sidgame-app/src/components/Main.js
+++ sidgame-app/src/components/Main.js
@@ -3,6 +3,10 @@ import { useState } from "react";
 export default function Main(props) {
   const [scoreMode, setScoreMode] = useState(false);
 
+  const toggleMode = function() {
+    setScoreMode(!scoreMode);
+  }
+
   return(<div>
     <button onClick={props.increaseScore}>Increase score</button>
     <button onClick={props.resetScore}>Reset score</button>
