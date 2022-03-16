60cf18d682f3b7962f10e099f7e24fc28d9964b4
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-16 13:47:20 -0400

    Add useState for select

--- sidgame-app/src/components/Buttons.js
+++ sidgame-app/src/components/Buttons.js
@@ -1,5 +1,6 @@
 export default function Buttons(props) {
   return(<div>
+    <br />
     <button onClick={props.increaseScore}>Increase score</button>
     <button onClick={props.decreaseScore}>Decrease score</button>
     <button onClick={props.resetScore}>Reset score</button>
--- sidgame-app/src/components/Main.js
+++ sidgame-app/src/components/Main.js
@@ -4,6 +4,7 @@ import Buttons from "./Buttons";
 
 export default function Main(props) {
   const [resultMode, setResultMode] = useState(false);
+  const [select, setSelect] = useState("");
 
   const toggleMode = function() {
     setResultMode(!resultMode);
