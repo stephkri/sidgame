908ab585cf4e4dd96d95038c4ddd3e183fe268ea
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-15 20:24:54 -0400

    Import score and setScore as props

--- sidgame-app/src/App.js
+++ sidgame-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <Score />
+          <Score score={score} setScore={setScore} />
         </p>
         <a
           className="App-link"
