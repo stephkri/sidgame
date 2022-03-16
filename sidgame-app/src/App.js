0388b0924a51fb9ec204eac00b640de0444eb5ba
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-15 20:26:30 -0400

    Import Main into App

--- sidgame-app/src/App.js
+++ sidgame-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import { useState } from "react";
 import './App.css';
+import Main from './components/Main';
 import Score from './components/Score';
 
 function App() {
