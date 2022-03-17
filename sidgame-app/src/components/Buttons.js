cc557630a88128e3ac023f85e0bf9ded27ef09d4
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-17 16:02:43 -0400

    Reformat onClick functions

--- sidgame-app/src/components/Buttons.js
+++ sidgame-app/src/components/Buttons.js
@@ -1,7 +1,13 @@
 export default function Buttons(props) {
   return(<div>
-    <button onClick={() => {props.setSelect("Hoffman"); props.toggleMode()}}>Sid Hoffman</button>
-    <button onClick={() => {props.setSelect("Frenchman"); props.toggleMode()}}>Sid Frenchman</button>
+    <button onClick={() => {
+      props.setSelect("Hoffman");
+      props.toggleMode();
+      }}>Sid Hoffman</button>
+    <button onClick={() => {
+      props.setSelect("Frenchman");
+      props.toggleMode()
+      }}>Sid Frenchman</button>
     <br />
     <button onClick={props.increaseScore}>Increase score</button>
     <button onClick={props.decreaseScore}>Decrease score</button>
