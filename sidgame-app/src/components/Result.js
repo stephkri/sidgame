f92edb966fff9ab1f11fac31b4fbd30ad771968c
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-17 16:19:11 -0400

    Remove placeholder text from Result

--- sidgame-app/src/components/Result.js
+++ sidgame-app/src/components/Result.js
@@ -1,6 +1,5 @@
 export default function Result(props) {
   return (<div>
-    <p>Here is the Result component</p>
     <p>Your selection: {props.select}</p>
     <p>The result: {props.result.lastName}</p>
     {props.result.addPoint ? <p>You gained a point</p> : <p>You did not gain a point</p>}
