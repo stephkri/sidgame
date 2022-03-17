8a10cb7f9b7c3469d2cc39ee29733baf76eeba8d
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-17 16:33:54 -0400

    Change score marker to h3

--- sidgame-app/src/components/Score.js
+++ sidgame-app/src/components/Score.js
@@ -1,6 +1,6 @@
 export default function Score(props) {
   return(<div>
-    <p>Here is the score: {props.score}</p>
+    <h3>Your score: {props.score}</h3>
     <button onClick={props.resetScore}>Reset score</button>
     </div>);
 }
