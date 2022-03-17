4718b0f808fea01a4bac1d3bc389ccf45e5c75d1
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-17 16:19:46 -0400

    Change to a heading with Sid

--- sidgame-app/src/components/Result.js
+++ sidgame-app/src/components/Result.js
@@ -1,7 +1,7 @@
 export default function Result(props) {
   return (<div>
+    <h1>Sid {props.result.lastName}</h1>
     <p>Your selection: {props.select}</p>
-    <p>The result: {props.result.lastName}</p>
     {props.result.addPoint ? <p>You gained a point</p> : <p>You did not gain a point</p>}
     <button onClick={props.toggleMode}>Toggle mode</button>
     </div>)
