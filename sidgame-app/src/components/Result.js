9810ed55173a96d95b857f65263fe6992c9db7d9
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-17 16:50:22 -0400

    Make result text a bit more complete

--- sidgame-app/src/components/Result.js
+++ sidgame-app/src/components/Result.js
@@ -1,7 +1,9 @@
 export default function Result(props) {
   return (<div>
     <h1>Sid {props.result.lastName}</h1>
-    {props.result.addPoint ? <p>You gained a point</p> : <p>You did not gain a point</p>}
+    {props.result.addPoint ?
+    <p>Congratulations! You gained a point.</p> :
+    <p>Sorry! You did not gain a point.</p>}
     <button onClick={props.toggleMode}>Next round</button>
     </div>)
 };
