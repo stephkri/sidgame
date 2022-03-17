1b76c5ef27779e311200581289454032c6ca977c
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-17 16:52:02 -0400

    Add class name to result texts

--- sidgame-app/src/components/Result.js
+++ sidgame-app/src/components/Result.js
@@ -2,8 +2,8 @@ export default function Result(props) {
   return (<div>
     <h1>Sid {props.result.lastName}</h1>
     {props.result.addPoint ?
-    <p>Congratulations! You gained a point.</p> :
-    <p>Sorry! You did not gain a point.</p>}
+    <p className={"win-point"}>Congratulations! You gained a point.</p> :
+    <p className={"lose-point"}>Sorry! You did not gain a point.</p>}
     <button onClick={props.toggleMode}>Next round</button>
     </div>)
 };
