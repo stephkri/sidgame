6cb31c51b4a38adad2b8b54518a1d05ed96280ec
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-17 16:34:39 -0400

    Change 'toggle mode' to 'Next round'

--- sidgame-app/src/components/Result.js
+++ sidgame-app/src/components/Result.js
@@ -2,6 +2,6 @@ export default function Result(props) {
   return (<div>
     <h1>Sid {props.result.lastName}</h1>
     {props.result.addPoint ? <p>You gained a point</p> : <p>You did not gain a point</p>}
-    <button onClick={props.toggleMode}>Toggle mode</button>
+    <button onClick={props.toggleMode}>Next round</button>
     </div>)
 };
