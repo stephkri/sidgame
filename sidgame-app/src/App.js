5956a7d356d421122186411def895e00a0fd0fc3
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-20 22:07:04 -0400

    Elaborate on score

--- sidgame-app/src/App.js
+++ sidgame-app/src/App.js
@@ -4,7 +4,12 @@ import Main from './components/Main';
 import Score from './components/Score';
 
 function App() {
-  // This is the game score, which is increased when the player guesses correctly.
+  /*
+  This is the game score, which is increased when the player guesses correctly.
+  The game functionality is in the Main component, so increaseScore is needed
+  to handle the click of the two option buttons. The actual showing of the score is done
+  in the Score component, which has an option to reset the player's score to zero.
+  */
   const [score, setScore] = useState(0);
 
   const increaseScore = function() {
